ab6da14f5af121e2d62f5adec5d05e09d14e4d71
kirilll.212 <mac@Kirils-MacBook-Pro-15.local>
2023-12-28 11:24:56 +0200

    permission file

---
+++ config/permission.ts
@@ -0,0 +1,17 @@
+module.exports = {
+    DELETING_POSTS: 'Deleting posts',
+    EDITING_POSTS: 'Editing posts',
+    CREATE_POSTS: 'Create post',
+    VIEW_POSTS: 'View post',
+    DELETING_USERS: 'Deleting users',
+    EDITING_USERS: 'Editing users',
+    VIEW_USERS: 'View users',
+    GRANT_OF_RIGHTS: 'Grant of rights',
+    EDITING_MANAGERS: 'Editing manager',
+    DELETING_MANAGERS: 'Deleting manager',
+    VIEW_MANAGERS: 'View manager',
+    DELETING_ROLES: 'Deleting role',
+    EDITING_ROLES: 'Editing role',
+    CREATE_ROLES: 'Create role',
+    VIEW_ROLES: 'View role'
+}
